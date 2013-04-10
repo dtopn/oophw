33d0637cda98ce56c04c2821ffb5f9aed7b8ea46
Tao J <tao-j@outlook.com>
2013-04-10 22:20:19 +0800

    save for assn02

--- assn03.cpp
+++ assn03.cpp
@@ -13,11 +13,12 @@ using std::string;
 
 class Room{
 public:
-	Room(const int rank, const int file):posi_rank(rank), posi_file(file), type("normal room"){printf("rank%dfile%d", posi_rank, posi_file);}
+	Room(const int rank, const int file):posi_rank(rank), posi_file(file){printf("rank%dfile%d", posi_rank, posi_file);}
 	//TODO assert(file >=2);
 	void set_near(Room *pt, int posi);
 	void messg();
 	Room* get_near(int posi);
+    void set_type();
 private:
 	Room *nearpt[MAXPOSI];//max 7 adjacent rooms
 	//regard 0 as left 1 as right 2 as up 3 as down
@@ -29,7 +30,7 @@ protected:
 };
 
 void Room::set_near(Room *pt, int posi){
-	nearpt[posi] = pt;
+    nearpt[posi] = pt;
 }
 
 Room* Room::get_near(int posi){
