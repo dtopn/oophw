7d12b746558d6e519b25590da788615c040390c3
Tao J <tao-j@outlook.com>
2013-05-03 17:25:05 +0000

    All complete, start debugging

---
+++ pdremove.cpp
@@ -0,0 +1,33 @@
+#include "datio.h"
+#include "heure.h"
+
+#include <string>
+using namespace std;
+
+int main (int argc, const char * argv[] ) {
+    DAT da;
+	iparse (da);
+    
+	if ( argc != 2) {
+        ostd("Wrong command\n");
+        exit(1);
+    }
+    if ( (strlen(argv[1]) != 10) ) {
+        ostd("Illeagal date\nEnter yyyy/mm/dd\n");
+        exit(1);
+    }
+    
+    long int day = conLheure(argv[1]);
+    
+    DAT::const_iterator map_i;
+    ostd(day);
+    map_i = da.find((DAT::key_type) day);
+    if (map_i == da.end()) {
+        ostd("No such entry.\n");
+        exit(-1);
+    }
+    
+    da.erase(map_i);
+    oparse(da);
+    return 0;
+}
